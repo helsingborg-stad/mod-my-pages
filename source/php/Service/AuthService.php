56871035a7720739a20dc1c07c2dab9ed9cb22e2
nRamstedt <nikolas.ramstedt@gmail.com>
2022-09-21 10:34:11 +0200

    prefix endpoint url

--- source/php/Service/AuthService.php
+++ source/php/Service/AuthService.php
@@ -9,7 +9,7 @@ class AuthService
     public static function token($args)
     {
         $queryArgs  = ['ts_session_id' => $args['ts_session_id']];
-        $requestUrl = add_query_arg($queryArgs, Settings::apiUrl() . '/auth/token');
+        $requestUrl = add_query_arg($queryArgs, Settings::apiUrl() . '/api/v1/auth/token');
         $request = curl_init($requestUrl);
         curl_setopt($request, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
