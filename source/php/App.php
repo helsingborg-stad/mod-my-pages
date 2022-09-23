b4ec00495696f3d0bade2282e9c37bd2973b663e
nRamstedt <nikolas.ramstedt@gmail.com>
2022-09-23 18:02:19 +0200

    expose menu items to views

--- source/php/App.php
+++ source/php/App.php
@@ -17,6 +17,7 @@ class App
         add_filter('Municipio/Navigation/Item', array($this, 'replaceMenuLabels'));
         add_filter('Municipio/blade/view_paths', array($this, 'addSearchableBladeTemplatePaths'), 1, 1);
         add_action('init', array($this, 'registerMyPagesMenu'), 5, 2);
+        add_filter('Municipio/viewData', array($this, 'myPagesMenuController'), 1, 1);
     }
 
     public function redirect()
@@ -79,5 +80,35 @@ class App
     public function registerMyPagesMenu()
     {
         register_nav_menu('my-pages-menu', __('My Pages Menu', MOD_MY_PAGES_TEXT_DOMAIN));
+    }
+
+    public function myPagesMenuController($data)
+    {
+        $redirectUrl = add_query_arg(['callbackUrl' => home_url('mina-sidor')], home_url('auth'));
+        $loginUrl = add_query_arg(['redirect_url' => $redirectUrl], \ModMyPages\Admin\Settings::apiUrl() . '/api/v1/auth/login');
+        $dropdownItems = array_map(
+            fn ($p) => [
+                'text' => $p->title,
+                'link' => $p->url,
+                'attributeList' => [
+                    'title' => $p->attr_title
+                ],
+                'classList' => $p->classes,
+            ],
+            wp_get_nav_menu_items(get_nav_menu_locations()['my-pages-menu'] ?? 0) ?? []
+        );
+
+        $data['myPagesMenu'] = [
+            'login'     => [
+                'text'      => __('Login', MOD_MY_PAGES_TEXT_DOMAIN),
+                'url'      => $loginUrl
+            ],
+            'dropdown'  => [
+                'text'      => \ModMyPages\Session\Profile::name(),
+                'items'     => $dropdownItems
+            ],
+        ];
+
+        return $data;
     }
 }
