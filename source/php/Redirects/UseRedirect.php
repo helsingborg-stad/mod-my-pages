85d8966f6d2d5296ac535d965deadbbed5e8ea3a
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-10-06 15:48:43 +0200

    temp: log redirects

--- source/php/Redirects/UseRedirect.php
+++ source/php/Redirects/UseRedirect.php
@@ -19,16 +19,31 @@ class UseRedirect
 
     public function enable(callable $callback): void
     {
+        $this->log("Current route: {$this->currentRoute}");
+        $this->log(PHP_EOL);
         foreach ($this->routes as $route => $handler) {
             $this->attachHandler($route, $handler, $callback);
         }
     }
 
+    public function log(string $message)
+    {
+        error_log(print_r($message, true));
+    }
+
     public function attachHandler(string $route, Types\IRedirectHandler $handler, callable $callback): void
     {
+        $this->log("Match route: {$route} ? {$this->matchRoute($route)}");
+        $this->log("Should redirect: {$route} ? {$handler->shouldRedirect($_GET)}");
         if ($this->matchRoute($route) && $handler->shouldRedirect($_GET)) {
+            $this->log("Execute redirect: {$route}");
+            $this->log(PHP_EOL);
             $callback($handler->redirectUrl($_GET));
+            return;
         }
+
+        $this->log("Skip redirect: {$route}");
+        $this->log(PHP_EOL);
     }
 
     public function matchRoute(string $route)
