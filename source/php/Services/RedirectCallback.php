a28341402ca6cc15113beee33ef5e0a38ff92949
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-10-07 17:20:03 +0200

    fix: bypass cache

--- source/php/Services/RedirectCallback.php
+++ source/php/Services/RedirectCallback.php
@@ -2,12 +2,14 @@
 
 namespace ModMyPages\Services;
 
+use ModMyPages\Helper\PageCache;
 use ModMyPages\Services\Types\IRedirectCallback;
 
 class RedirectCallback implements IRedirectCallback
 {
     public function __invoke(string $redirectUrl): void
     {
+        PageCache::bypass();
         wp_redirect($redirectUrl);
         exit;
     }
