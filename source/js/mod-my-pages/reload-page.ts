680d4cc8c4d12fb1854a4a2f328c5266627bdc63
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-11-10 12:43:24 +0100

    append query arg to url before reloading

--- source/js/mod-my-pages/reload-page.ts
+++ source/js/mod-my-pages/reload-page.ts
@@ -3,16 +3,41 @@ import { getAccessToken } from "../gdi-host/access-token";
 export const reloadPageWhenTokenExpires = async (frequencyInMs: number) => {
     const delayPromise = (delayInMs: number): Promise<void> =>
         new Promise((resolve) => setTimeout(resolve, delayInMs))
-
     const timestampInSeconds = (): number =>
         Math.floor(Date.now() / 1000);
+
+    const reloadPage = () => {
+        const getQueryParams = (url: string): URLSearchParams =>
+            (new URL(url)).searchParams;
+        const withQueryParams = (queryParams: Record<string, string>) =>
+            (params: URLSearchParams): URLSearchParams => {
+                Object.entries(queryParams)
+                    .forEach(([key, value]) => {
+                        params.set(key, value);
+                    })
+                return params;
+            };
+        const toQueryString = (params: URLSearchParams): string =>
+            `?${params.toString()}`;
+
+        window.location.href = [
+            `${window.location.protocol}//`,
+            window.location.host,
+            window.location.pathname,
+            toQueryString(
+                withQueryParams({ 'notice': 'inactive-signout' })(
+                    getQueryParams(window.location.href)
+                )
+            )
+        ].join('')
+    }
 
     const tryReloadPageRecursively = (): Promise<void> =>
         delayPromise(frequencyInMs)
             .then(getAccessToken)
             .then(({ expires }) =>
                 timestampInSeconds() >= expires
-                    ? location.reload()
+                    ? reloadPage()
                     : tryReloadPageRecursively()
             );
 
