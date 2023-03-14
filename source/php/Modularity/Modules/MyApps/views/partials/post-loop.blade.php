0bf86aab1676155d4403f86527d9c2d9d194f47f
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-03-14 12:48:20 +0100

    simplify

--- source/php/Modularity/Modules/MyApps/views/partials/post-loop.blade.php
+++ source/php/Modularity/Modules/MyApps/views/partials/post-loop.blade.php
@@ -1,10 +1,9 @@
 <div class="o-grid o-grid--half-gutter">
     @foreach ($viewModel->items as $item)
-        @if ($item->display === 'hide')
-            @continue
+        @if ($item->display !== 'hide')
+            <div class="o-grid-6 o-grid-6@sm o-grid-4@md u-display--flex">
+                @include('partials.post-card')
+            </div>
         @endif
-        <div class="o-grid-6 o-grid-6@sm o-grid-4@md u-display--flex">
-            @include('partials.post-card')
-        </div>
     @endforeach
 </div>
